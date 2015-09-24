dd9e6dc8b30edfcb5b4ddaa9356d6ef2c03320ff
Jacob Atzen <jatzen@gmail.com>
2015-09-24 16:44:12 +0200

    Player can join table

--- table.elm
+++ table.elm
@@ -1,21 +1,42 @@
 module Table where
 
 import Deck exposing (Deck)
+import Array exposing (Array)
 
 type alias Money = Int
 
 type alias Player = {
   name : String,
-  stack : Money
+  stack : Money,
+  uuid : String
 }
 
 type alias Seat = Maybe Player
 
 type alias Table = {
-  seats : List Seat,
+  seats : Array Seat,
   pot : Int
 }
 
 type alias Game = {
   deck : Deck
 }
+
+newTable : Int -> Table
+newTable seatCount =
+  Table (Array.repeat seatCount Nothing) 0
+
+availableSeatIndex table =
+  let
+      list = Array.toIndexedList table.seats
+      availableSeats = List.filter (\(index, seat) -> seat == Nothing) list
+      firstSeat = List.head availableSeats
+  in
+      case firstSeat of
+        Nothing -> Nothing
+        Just (index, _) -> Just index
+
+join table player =
+  case availableSeatIndex table of
+    Nothing -> table
+    Just index -> { table | seats <- Array.set index player table.seats }
